27e0f4681ac30200932f7bc1d47e64f4e3646e36
JimmyTsai <qaz3182@gmail.com>
2020-05-07 01:53:45 +0800

    add api post update

--- app/api/api_v0/posts.rb
+++ app/api/api_v0/posts.rb
@@ -33,5 +33,22 @@ module ApiV0
       present post, with: ApiV0::Entities::Post
     end
 
+    desc "Update post"
+    params do
+      requires :id, type: String, desc: 'Post ID.'
+      requires :title, type: String, desc: "Post title"
+      requires :context, type: String, desc: "Post context"
+    end
+    patch "/posts/:id" do
+      post = current_user.posts.find_by(id: params[:id])
+
+      if post.update(permitted_params)
+        present post, with: ApiV0::Entities::Post
+      else
+        raise StandardError, $!
+      end
+    end
+
+
   end
 end
